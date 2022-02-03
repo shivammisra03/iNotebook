8ec0d2678af7b40f1fbc792fb5e3e146e100e7d2
Shivam Misra <shivam.misra03@gmail.com>
2022-02-03 21:50:28 +0530

    react : tutorial 44

---
+++ backend/models/Notes.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+
+
+
+const NotesSchema = new Schema({
+    titile: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        default: 'General'
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('notes', NotesSchema)
