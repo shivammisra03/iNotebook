06c922a2138fe8745fd03a8531102b5eedd269e6
Shivam Misra <shivam.misra03@gmail.com>
2022-02-04 11:49:10 +0530

    react : tutorial-45

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const router = express.Router()
 
-router.get('/', (req, res) => {
-    obj = {
-        name : 'shivam',
-        id : 10
-    }
-    res.json(obj)
+const express = require('express')
+const { Mongoose } = require('mongoose')
+const router = express.Router()
+const User = require('../models/User')
+
+//create a user using POST : '/api/auth/' doesnt require auth
+router.post('/', (req, res) => {
+
+    const user = User(req.body)
+    user.save()
+    console.log(req.body)
+    res.send(req.body)
+
 })
 
 
