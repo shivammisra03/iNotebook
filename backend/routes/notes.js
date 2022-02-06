6dd44765cade4f20d8ad2fea86abb7bc93e78ec5
Shivam Misra <shivam.misra03@gmail.com>
2022-02-06 16:50:14 +0530

    react : tutorial 52

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,43 @@
 const express = require('express')
 const router = express.Router()
+const fetchuser = require('../middleware/fetchuser')
+const Notes = require('../models/Notes')
+const { body, validationResult } = require('express-validator');
 
-router.use('/', (req, res) => {
-    res.json([])
+//Route 1 : Get all the notes
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id })
+        res.json(notes)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Internal Server error occured')
+    }
+
+})
+
+
+//Route 2 : Add a new note using POST '/api/auth/addnote', login required
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { title, description, tag } = req.body;
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save();
+        res.send(savedNote)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Internal Server error occured')
+    }
+
 })
 
 module.exports = router
