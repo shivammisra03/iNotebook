3ad137d0a29ca8ff382b38e236d8e2053c1ceb75
Shivam Misra <shivam.misra03@gmail.com>
2022-02-10 21:41:47 +0530

    react : tutorial 60

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,6 +3,36 @@ import NoteContext from './noteContext';
 
 
 const NoteState = (props) => {
+
+    const notesInitial = [
+        {
+            "_id": "620112bf9a21612b1405deba",
+            "user": "61fe7451e52419a8123a5d01",
+            "title": "my first title",
+            "description": "kuch bhi",
+            "tag": "General",
+            "timestamp": "2022-02-07T12:38:23.371Z",
+            "__v": 0
+        },
+        {
+            "_id": "62053722ec245f1d1409a48e",
+            "user": "61fe7451e52419a8123a5d01",
+            "title": "my second title",
+            "description": "kuch bhi returns",
+            "tag": "General",
+            "timestamp": "2022-02-10T16:02:42.488Z",
+            "__v": 0
+        },
+        {
+            "_id": "6205374bec245f1d1409a491",
+            "user": "61fe7451e52419a8123a5d01",
+            "title": "my third title",
+            "description": "kya kre bhai",
+            "tag": "General",
+            "timestamp": "2022-02-10T16:03:23.596Z",
+            "__v": 0
+        }
+    ]
     // const s1 = {
     //     "name": 'Shivam',
     //     "class": "5b"
@@ -17,8 +47,10 @@ const NoteState = (props) => {
     //         })
     //     }, 1000);
     // }
+
+    const [notes, setNotes] = useState(notesInitial)
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
